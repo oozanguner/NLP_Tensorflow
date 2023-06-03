9f9dc5935f0a049272c78f2dd927ab76591f5341
Ozan Güner <oktayozanguner@hotmail.com>
2023-06-03 15:01:22 +0200

    functions file has been updated

--- functions.py
+++ functions.py
@@ -1,7 +1,134 @@
 import matplotlib.pyplot as plt
+import csv
+from keras.preprocessing.text import Tokenizer
+from keras.utils import pad_sequences
+import tensorflow as tf
 
 def plot_metrics(history, metric="loss"):
     plt.plot(history.history[metric], label=metric)
     plt.plot(history.history["val_" + metric], label="val_" + metric)
     plt.legend()
     plt.show()
+
+def parse_data_from_file(filename):
+    """
+    Extracts sentences and labels from a CSV file
+    
+    Args:
+        filename (string): path to the CSV file
+    
+    Returns:
+        sentences, labels (list of string, list of string): tuple containing lists of sentences and labels
+    """
+
+    sentences = []
+    labels = []
+    
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        
+        for row in reader:
+            sentences.append(row[5])
+            labels.append([row[0]])
+            
+        labels = [0 if l == '0' else 1 for l in labels]
+        
+    return sentences, labels
+
+def train_val_split(sentences, labels, training_split):
+    """
+    Splits the dataset into training and validation sets
+    
+    Args:
+        sentences (list of string): lower-cased sentences without stopwords
+        labels (list of string): list of labels
+        training split (float): proportion of the dataset to convert to include in the train set
+    
+    Returns:
+        train_sentences, validation_sentences, train_labels, validation_labels - lists containing the data splits
+    """   
+    
+    # Compute the number of sentences that will be used for training (should be an integer)
+    train_size = int(training_split * len(sentences))
+
+    # Split the sentences and labels into train/validation splits
+    train_sentences = sentences[:train_size]
+    train_labels = labels[:train_size]
+
+    validation_sentences = sentences[train_size:]
+    validation_labels = labels[train_size:]
+    
+    return train_sentences, validation_sentences, train_labels, validation_labels
+
+def fit_tokenizer(train_sentences, oov_token):
+    """
+    Instantiates the Tokenizer class on the training sentences
+    
+    Args:
+        train_sentences (list of string): lower-cased sentences without stopwords to be used for training
+        oov_token (string) - symbol for the out-of-vocabulary token
+    
+    Returns:
+        tokenizer (object): an instance of the Tokenizer class containing the word-index dictionary
+    """    
+    
+    # Instantiate the Tokenizer class, passing in the correct values for oov_token
+    tokenizer = Tokenizer(oov_token=oov_token)
+    
+    # Fit the tokenizer to the training sentences
+    tokenizer.fit_on_texts(train_sentences)
+    
+    return tokenizer
+
+def seq_pad_and_trunc(sentences, tokenizer, padding, truncating, maxlen):
+    """
+    Generates an array of token sequences and pads them to the same length
+    
+    Args:
+        sentences (list of string): list of sentences to tokenize and pad
+        tokenizer (object): Tokenizer instance containing the word-index dictionary
+        padding (string): type of padding to use
+        truncating (string): type of truncating to use
+        maxlen (int): maximum length of the token sequence
+    
+    Returns:
+        pad_trunc_sequences (array of int): tokenized sentences padded to the same length
+    """     
+       
+    # Convert sentences to sequences
+    sequences = tokenizer.texts_to_sequences(sentences)
+    
+    # Pad the sequences using the correct padding, truncating and maxlen
+    pad_trunc_sequences = pad_sequences(sequences, maxlen=maxlen, padding=padding, truncating=truncating)
+    
+    return pad_trunc_sequences
+
+
+def create_model_with_transfer_learning(vocab_size, embedding_dim, maxlen, embeddings_matrix):
+    """
+    Creates a binary sentiment classifier model
+    
+    Args:
+        vocab_size (int): size of the vocabulary for the Embedding layer input
+        embedding_dim (int): dimensionality of the Embedding layer output
+        maxlen (int): length of the input sequences
+        embeddings_matrix (array): predefined weights of the embeddings
+    
+    Returns:
+        model (tf.keras Model): the sentiment classifier model
+    """
+    
+    model = tf.keras.Sequential([ 
+        # This is how you need to set the Embedding layer when using pre-trained embeddings
+        tf.keras.layers.Embedding(vocab_size+1, embedding_dim, input_length=maxlen, weights=[embeddings_matrix], trainable=False),
+        tf.keras.layers.Conv1D(64, 5, activation = 'relu'),
+        tf.keras.layers.GlobalMaxPooling1D(),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+    
+    model.compile(loss="binary_crossentropy",
+                  optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+                  metrics=['accuracy']) 
+
+    return model
